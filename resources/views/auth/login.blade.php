309eefa48b26fd1dd5ebb2278435c6b882839767
Guillermin <olmedo.guillermo@esim.edu.ar>
2025-06-02 20:25:24 -0300

    versão com visualizações

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,13 @@
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Iniciar sesióneeeeeee</title>
+    <title>Iniciar sesión</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
-
+    {{-- Opcional: Estilos personalizados para tu formulario --}}
     <style>
         :root {
             --color-primary: #567A60;
@@ -29,7 +28,7 @@
         }
         
         .login-container {
-            background-color:rgb(223, 52, 52);
+            background-color: #ffffff;
             padding: 30px;
             border-radius: 12px;
             box-shadow: 0 8px 20px rgba(41, 59, 46, 0.15);
